d84e372e176c9e1d2f51898b8298807d3211094e
Hambat <xambat@mail.ru>
2017-06-28 19:07:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,54 +1,24 @@
-var state = 0,
-    input = document.getElementById('inp'),
-    stopBtn = document.getElementById('stopBtn'),
-    playBtn = document.getElementById('playBtn'),
-    decrementBtn = document.getElementById('decrementBtn');
-
-function updateState(state, action) {
-  if(action.type === 'INCREMENT') {
-    return state + action.amount;
+class Test {
+  constructor(value) {
+       this.value = value;
   }
-  if(action.type === 'DECREMENT') {
-      return state - action.amount;
+    
+  a = () => {
+    console.log(this.value)
+  } 
+  
+  b() {
+    console.log(this.value)
   }
-   else {
-    return state;
+
+  c() {
+    console.log(42)
   }
-};
 
-//increment state
-var increment = {type: 'INCREMENT', amount: 1};
+}
 
-var intervalFunc = function() {
-  state = updateState(state, increment);
-  input.value = state;
-};
+const test = new Test(23);
 
-var intervalID = setInterval(intervalFunc, 1000);
-
-//stop state
-stopBtn.onclick = function() {
-  clearInterval(intervalID);
-};
-
-//play state
-playBtn.onclick = function() {
-  clearInterval(intervalID);
-  intervalID = setInterval(intervalFunc, 1000);
-
-  var alma = input.value;
-};
-
-//decrement state
-var decrement = {type: 'DECREMENT', amount: 1};
-
-var intervalFuncDec = function() {
-  state = updateState(state, decrement);
-  input.value = state;
-};
-
-
-decrementBtn.onclick = function() {
-  clearInterval(intervalID);
-  intervalID = setInterval(intervalFuncDec, 1000);
-};
+setTimeout(test.a, 1000);
+setTimeout(test.b, 1000);
+setTimeout(test.c(), 1000);
